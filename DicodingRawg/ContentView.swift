7ed29ee1cccb7e4da80f26977424f5f992aac539
Najwan160 <najwan160@gmail.com>
2022-12-17 14:59:24 +0700

    Initial Commit

---
+++ DicodingRawg/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DicodingRawg
+//
+//  Created by Muhammad Najwan Latief on 17/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DicodingRawg/DicodingRawgApp.swift
@@ -0,0 +1,17 @@
+//
+//  DicodingRawgApp.swift
+//  DicodingRawg
+//
+//  Created by Muhammad Najwan Latief on 17/12/22.
+//
+
+import SwiftUI
+
+@main
+struct DicodingRawgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
